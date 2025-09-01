838b40510756ed2c1a579b257cb8d63002539136
MaruDeHabanero <jose13.maciasm@gmail.com>
2025-08-31 20:44:31 -0600

    fixed the SqliteConnection

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74abae41b55df903eee7400aaa8f9092657e88c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d17f9b01ac188d9e69401c2b01d33d141095d141")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
