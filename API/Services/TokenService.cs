52ac24589a54fac52f5d0dfb9cc7037a2885c33b
MaruDeHabanero <jose13.maciasm@gmail.com>
2025-09-12 12:35:05 -0600

    Parcial02 - Add TokenService

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,3 +1,4 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using API.Entities;
@@ -24,7 +25,17 @@ public class TokenService(IConfiguration configuration) : ITokenService
             new (ClaimTypes.NameIdentifier, user.Id),
 
         };
-        
+
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var tokenDescription = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescription);
+
+        return tokenHandler.WriteToken(token);
     }
 }
