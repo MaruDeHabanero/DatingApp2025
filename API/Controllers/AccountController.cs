dbc2a0b37e4b9d9cd7af516cfca3e366568d5e6e
MaruDeHabanero <jose13.maciasm@gmail.com>
2025-09-12 08:26:49 -0600

    Parcial02 - Added DTOs

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(RegisterRequest request)
+    {
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            Email = request.Email,
+            DisplayName = request.DisplayName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    } 
+}   
