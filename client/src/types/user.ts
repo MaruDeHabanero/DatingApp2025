d2ed332dd66c897ffb8c9d0c6f37082982c265b1
MaruDeHabanero <jose13.maciasm@gmail.com>
2025-09-23 07:46:02 -0600

    Parcial02 - Add Persistence to login User

---
+++ client/src/types/user.ts
@@ -0,0 +1,26 @@
+export type User = {
+  id: string;
+  displayName: string;
+  email: string;
+  token: string;
+  imageUrl: string;
+};
+
+export interface IUser {
+  id: string;
+  displayName: string;
+  email: string;
+  token: string;
+  imageUrl: string;
+}
+
+export type LoginCreds = {
+  email: string;
+  password: string;
+};
+
+export type RegisterCreds = {
+  displayName: string;
+  email: string;
+  password: string;
+};
