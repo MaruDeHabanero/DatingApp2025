97b1116af3640a64ae9af06399f2348ce61ec578
MaruDeHabanero <jose13.maciasm@gmail.com>
2025-10-09 07:25:12 -0600

    Parcial02 - Add error handling, not-found

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
 import { authGuard } from '../core/guard/auth-guard';
 import { TestErrors } from '../test-errors/test-errors';
+import { NotFound } from '../shared/errors/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -22,5 +23,5 @@ export const routes: Routes = [
     ],
   },
   { path: "errors", component: TestErrors },
-  { path: '**', component: Home },
+  { path: '**', component: NotFound },
 ];
