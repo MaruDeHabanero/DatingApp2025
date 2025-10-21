ff312a1d1fea28a3c83722f4c008e775f3d2b259
MaruDeHabanero <jose13.maciasm@gmail.com>
2025-10-21 08:21:12 -0600

    Parcial02 - Implement member repo in controller

--- API/Entities/Member.cs
+++ API/Entities/Member.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace API.Entities;
 
@@ -17,7 +18,9 @@ public class Member
 
     //Navigation properties
     [ForeignKey(nameof(Id))]
+    [JsonIgnore]
     public AppUser User { get; set; } = null!;
 
+    [JsonIgnore]
     public List<Photo> Photos { get; set; } = [];
 }
