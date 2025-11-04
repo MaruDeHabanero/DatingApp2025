519b27275c507f290acca336349082091ed566d1
MaruDeHabanero <jose13.maciasm@gmail.com>
2025-11-04 08:48:40 -0600

    Parcial03 - Add theme manager

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,26 +1,44 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { themes } from '../theme';
 
 @Component({
   selector: 'app-nav',
   imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
-  styleUrl: './nav.css'
+  styleUrl: './nav.css',
 })
-export class Nav {
+
+export class Nav implements OnInit {
   protected accountService = inject(AccountService);
   protected creds: any = {};
   private router = inject(Router);
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+  protected themes = themes;
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+
+  handleSelectedTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+    const elem = document.activeElement as HTMLDivElement;
+    if (elem) {
+      elem.blur();
+    }
+  }
 
   login(): void {
     this.accountService.login(this.creds).subscribe({
-      next: response => {
+      next: (response) => {
         this.router.navigateByUrl('/members');
         this.creds = {};
       },
-      error: error => alert(error.message)
+      error: (error) => alert(error.message),
     });
   }
 
