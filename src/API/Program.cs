fbc2d7d36930b8b2ada138c490f0931eb9f64c97
MaruDeHabanero <jose13.maciasm@gmail.com>
2026-05-19 08:40:01 -0600

    Parcial06 - IDENTITY - Setup Identity

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -2,11 +2,13 @@ using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Text.Json.Serialization;
 using API.Data;
+using API.Entities;
 using API.Helpers;
 using API.Interfaces;
 using API.Middlewares;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using NSwag;
@@ -36,46 +38,8 @@ public static class Program
 
         AddDbContext(builder);
         AddScopedServices(builder);
-
-        var allowedOrigins = new[]
-        {
-            "http://localhost:4200",
-            "https://localhost:4200"
-        };
-
-        builder.Services.AddCors(options =>
-        {
-            options.AddPolicy("SpaCors", policy =>
-            {
-                policy.WithOrigins(allowedOrigins)
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-            });
-        });
-
-        builder.Services.AddOpenApiDocument(options =>
-        {
-            options.PostProcess = document =>
-            {
-                document.Info = new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Dating API",
-                    Description = "An ASP.NET Core Web API for managing Dating items",
-                    TermsOfService = "https://example.com/terms",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Example Contact",
-                        Url = "https://example.com/contact"
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Example License",
-                        Url = "https://example.com/license"
-                    }
-                };
-            };
-        });
+        AddOpenApiDocument(builder);
+        AddIdentity(builder);
 
         WebApplication app = builder.Build();
 
@@ -154,4 +118,42 @@ public static class Program
         builder.Services.AddScoped<UserActivityLogger>();
         builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
     }
-}   
+
+    private static void AddOpenApiDocument(WebApplicationBuilder builder)
+    {
+        builder.Services.AddOpenApiDocument(options =>
+        {
+            options.PostProcess = document =>
+            {
+                document.Info = new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Dating API",
+                    Description = "An ASP.NET Core Web API for managing Dating items",
+                    TermsOfService = "https://example.com/terms",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Url = "https://example.com/contact"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = "https://example.com/license"
+                    }
+                };
+            };
+        });
+    }
+
+    private static void AddIdentity(WebApplicationBuilder builder)
+    {
+        builder.Services.AddIdentityCore<AppUser>(opt =>
+        {
+            opt.Password.RequireNonAlphanumeric = false;
+            opt.User.RequireUniqueEmail = true;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<AppDbContext>();
+    }
+}
