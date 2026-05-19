c4d48cd80835ffcebadcdbf28a251b898f36cbf2
MaruDeHabanero <jose13.maciasm@gmail.com>
2026-05-19 08:40:08 -0600

    Parcial06 - IDENTITY - Add SECURITY to token with cookies

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -64,6 +64,14 @@ public static class Program
 
         if (app.Environment.IsDevelopment())
         {
+            app.UseCors(x => x.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins(
+                "http://localhost:4200",
+                "https://localhost:4200"
+            ));
+
             app.UseDeveloperExceptionPage();
             app.UseOpenApi();
             app.UseSwaggerUi();
