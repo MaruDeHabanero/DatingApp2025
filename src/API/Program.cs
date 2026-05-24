9885c3483c82592e435914bf40382619e7aaa5bb
MaruDeHabanero <jose13.maciasm@gmail.com>
2026-05-23 19:45:35 -0600

    Parcial06 - SignalR - Add message hub

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -86,6 +86,7 @@ public static class Program
         app.UseAuthorization();
         app.MapControllers();
         app.MapHub<PresenceHub>("hubs/presence");
+        app.MapHub<MessageHub>("hubs/messages");
         app.Run();
     }
 
